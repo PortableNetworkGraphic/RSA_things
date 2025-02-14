388aafd0aa966c6d109fd893fb29a87055846898
Mellified Lemon <ceilnicierega@proton.me>
2025-02-14 12:18:15 +0000

    Killed AES, for now. It's just too complicated. I need to learn more maths shit. :3

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from secrets import randbits
-
-from functions import *
 from primatives.sym_keyed_primatives.block_ciphers import AES
-from random import getrandbits, random
-
 
 def main():
 
     key = 181901790505226098135696675519197758418
 
-    print(AES.ShiftRows(0x32C1D6F2A7F91E7634BA9C8D4F251D68))
+    aes = AES(key)
+    print(hex(key))
+    print([[hex(s) for s in d] for d in aes.key])
 
 
 if __name__ == '__main__':
